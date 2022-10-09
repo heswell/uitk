31120eda2c66f52a3788c761188e91b152c189be
heswell <steve@heswell.com>
2022-10-09 18:43:33 +0100

    Tabs - remove console log and unnecessary useCallback

--- packages/lab/src/tabs/Tab.tsx
+++ packages/lab/src/tabs/Tab.tsx
@@ -132,10 +132,6 @@ export const Tab = forwardRef(function Tab(
     setCloseHover(false);
   };
 
-  const handleMouseDown = (e: MouseEvent<HTMLElement>): void => {
-    onMouseDown?.(e);
-  };
-
   const getLabel = () => {
     if (editable) {
       return (
@@ -183,7 +179,7 @@ export const Tab = forwardRef(function Tab(
       onFocus={handleFocus}
       onKeyDown={handleKeyDownMain}
       onKeyUp={handleKeyUp}
-      onMouseDown={handleMouseDown}
+      onMouseDown={onMouseDown}
       ref={setForkRef}
       role="tab"
       tabIndex={tabIndex}
